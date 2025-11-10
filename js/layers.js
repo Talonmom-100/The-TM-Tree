137087be76da370bfde349c0766b40b3bb8b4dd7
Talonmom-100 <lil_chanti50@hotmail.com>
2025-11-10 10:56:56 -0700

    USELESS
    ...
    
    (I did get my tree working now, because it was almost gone!)

--- js/layers.js
+++ js/layers.js
@@ -162,7 +162,7 @@ addLayer("t", {
         return new Decimal(1)
     },
 
-    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+    layerShown() { return true },      // Returns a bool for if this layer's node should be visible in the tree.
 
     upgrades: {
         11: {
